c484ae4b9cf8fcc037afda6ded22ce13d935b70a
jonathanlurie <lurie.jo@gmail.com>
2019-03-09 22:13:40 +0100

    ADD more markdown features

--- src/md2html.js
+++ src/md2html.js
@@ -1,9 +1,29 @@
+import hljs from 'highlight.js'
 import MarkdownIt from 'markdown-it/dist/markdown-it.js'
+import MditFootnote from 'markdown-it-footnote'
+import MditSub from 'markdown-it-sub'
+import MditSup from 'markdown-it-sup'
 
 const mdit = new MarkdownIt({
   html: true,
-  linkify: true
+  linkify: true,
+  typographer: true,
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value;
+      } catch (__) {}
+    }
+
+    return ''; // use external default escaping
+  }
 })
+
+// plugin
+mdit.use(MditFootnote)
+mdit.use(MditSub)
+mdit.use(MditSup)
+
 
 // TODO: add syntax highlighter
 
